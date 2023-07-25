3816f29fb42eb559093b31364ae3bd7706cb2254
AxlSou <axlsoubielle@gmail.com>
2023-07-25 18:57:47 +0100

    api call changed and env variable modified

--- src/services/client.js
+++ src/services/client.js
@@ -2,23 +2,24 @@ const URL_API_JOBS = `https://jsearch.p.rapidapi.com/search?query=`;
 const OPTIONS = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-    'X-RapidAPI-Host': process.env.RAPIDAPI_HOST
+    'X-RapidAPI-Key': import.meta.env.RAPIDAPI_KEY,
+    'X-RapidAPI-Host': import.meta.env.RAPIDAPI_HOST
   }
 };
 
 export const getJobsList = async (searchParams) => {
-    const formatedLocation = encodeURIComponent(searchParams.location)
-    // const formatedEmploymentTypes = encodeURIComponent(searchParams.employmentTypes)
-    const formatedQuery = encodeURIComponent(searchParams.curretQuery)
-    const url = `${URL_API_JOBS}${formatedQuery}%20in%20${formatedLocation}&page=${searchParams.currentPage}&num_pages=1`
+  const formatedLocation = encodeURIComponent(searchParams.location)
+  // const formatedEmploymentTypes = encodeURIComponent(searchParams.employmentTypes)
+  const formatedApiPage = encodeURIComponent(searchParams.apiPage)
+  const formatedQuery = encodeURIComponent(searchParams.currentQuery)
+  const url = `${URL_API_JOBS}${formatedQuery}%20in%20${formatedLocation}&page=${formatedApiPage}&num_pages=1`
 
-    try {
-      const response = await fetch(url, OPTIONS);
-      const result = await response.text();
-      return JSON.parse(result).data
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const response = await fetch(url, OPTIONS);
+    const result = await response.text();
+    return JSON.parse(result).data
+  } catch (error) {
+    console.error(error);
+  }
 }
 
